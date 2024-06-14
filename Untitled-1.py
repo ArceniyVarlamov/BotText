b63df54f8ac961dceae1c98b31b7c9ce14e873ec
tpgolovina <ptango06@gmail.com>
2024-06-14 20:34:04 +0300

    вторая версия

--- Untitled-1.py
+++ Untitled-1.py
@@ -24,5 +24,6 @@ def bot_restrict(file):
     return data
 account = YandexGPTLite('b1ghqjvoshov8q79s7f5', 'y0_AgAAAABnGlHCAATuwQAAAAEHgGuyAAD7npjmEllKNbG9uHGyh9At4qiK-g' )
 zapros=input()
+yandex_gpt_thread = YandexGPTThread(config_manager=YandexGPTConfigManagerForIAMToken(), messages=[{'role': 'system', 'text': 'You are a helpful assistant.'}, {'role': 'user', 'text': 'Hello!'}])
 text = account.create_completion(f'Твой персонаж: {bot_character("характер.txt")}, твои навыки:{bot_skills("навыки.txt")}, ограничения, которых ты должен придерживаться: {bot_restrict("ограничения.txt")}, твоя задача очеловечить данный текст f{zapros}', '1', system_prompt = 'отвечай на русском')
 print(text.replace('*',''))
